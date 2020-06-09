c605281ecfbe983adb64fe474517dcc1e61f8fcd
zhangxiongfei <zhangxiongfei@cogo.club>
2020-06-09 18:59:44 +0800

    ++

---
+++ IdeaProjects/leetCode/src/剑指offer/_面试题46_把数字翻译成字符串.java
@@ -0,0 +1,52 @@
+package 剑指offer;
+
+/*
+给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法。
+
+         
+
+        示例 1:
+
+        输入: 12258
+        输出: 5
+        解释: 12258有5种不同的翻译，分别是"bccfi", "bwfi", "bczi", "mcfi"和"mzi"
+
+        来源：力扣（LeetCode）
+        链接：https://leetcode-cn.com/problems/ba-shu-zi-fan-yi-cheng-zi-fu-chuan-lcof
+        著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处
+ */
+
+public class _面试题46_把数字翻译成字符串 {
+
+    /**
+     *
+     * 动态规划
+     * 当 num < 10时，只有一个结果
+     *
+     * 三个步骤
+     * 定义状态
+     *  dp[i] 表示截止 i位，
+     *
+     *
+     * */
+
+    public int translateNum(int num) {
+        if (num == 0) return 0;
+
+        String string = String.valueOf(num);
+        int count = string.length();
+        int[] dp = new int[count + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= count; i++) {
+            char pre = string.charAt(i - 2);
+            char cur = string.charAt(i - 1);
+            if (pre == '0' || pre > '2' || (pre == '2' && cur > '5')){
+                dp[i] = dp[i - 1];
+            }else {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+        }
+        return dp[count];
+    }
+}
