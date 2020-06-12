82628d5943673c2421a4d55d3ab6dd5f49d06617
zhangxiongfei <zhangxiongfei@cogo.club>
2020-06-12 18:15:53 +0800

    ++

--- IdeaProjects/leetCode/src/双指针/_15_三数之和.java
+++ IdeaProjects/leetCode/src/双指针/_15_三数之和.java
@@ -28,43 +28,62 @@ import java.util.List;
 
 public class _15_三数之和 {
 
+    /**
+     *
+     * 暴力法
+     * 三层循环
+     * 找到每一种可能， 和为0， 添加到结果数组
+     *
+     * 时间复杂度 : O(n ^ 3)
+     * 空间复杂度 : O(1)
+     *
+     * */
+
+
+
+    /**
+     *
+     * 先排序
+     * 排序后，数组两层循环
+     * 取到最外层某个数num
+     *    开始计算其后边 n - 1个元素的和为 -num 的组合
+     *    后边的操作 类似 排序版本 的两数之和
+     *    头尾双指针begain, end，
+     *      当和 == -num时, 加入结果数组
+     *      当和 < -num时, begain ++
+     *      当和 > -num时, end --
+     *
+     *
+     * */
     public static List<List<Integer>> threeSum(int[] nums) {
 
-        List<List<Integer>> res = new ArrayList<>();
+        // 先排序
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            // 为避免前边数字重复
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            int num = nums[i];
+        List<List<Integer>> ls = new ArrayList<>();
 
-            int begain = i + 1;
-            int end = nums.length - 1;
-            while (begain < end){
-                // 需要和上一次枚举的数不相同
-                if (nums[end] == nums[end - 1]) {
-                    continue;
-                }
+        // 排序以后的操作 跟 排序后的两数之和一样
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {  // 跳过可能重复的答案
 
-                int l = nums[begain];
-                int r = nums[end];
-
-                if (l + r + num == 0){
-                    List<Integer> tmp = new ArrayList<>();
-                    tmp.add(num);
-                    tmp.add(l);
-                    tmp.add(r);
-                    res.add(tmp);
-                }else if (l + r + num > 0){
-                    end --;
-                }else {
-                    begain ++;
+                int l = i + 1, r = nums.length - 1, sum = 0 - nums[i];
+                while (l < r) {
+                    if (nums[l] + nums[r] == sum) {
+                        ls.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                        while (l < r && nums[l] == nums[l + 1]) l++;
+                        while (l < r && nums[r] == nums[r - 1]) r--;
+                        l++;
+                        r--;
+                    } else if (nums[l] + nums[r] < sum) {
+                        while (l < r && nums[l] == nums[l + 1]) l++;   // 跳过重复值
+                        l++;
+                    } else {
+                        while (l < r && nums[r] == nums[r - 1]) r--;
+                        r--;
+                    }
                 }
             }
         }
-
-        return res;
+        return ls;
     }
 
     public static void main(String[] args) {
---
+++ mdsyncs/What is Cocoapods.md
@@ -0,0 +1,27 @@
+---
+title: "CocoaPods简介"
+date: 2020-06-12T11:16:41+08:00
+draft: true
+tags: ["iOS", "Cocoapods"]
+---
+
+本文是笔者对Cocoapods官网的翻译, 翻译水平有限, 可能会有误差。 准确信息请以官网为准。奉上[官网](https://cocoapods.org)链接
+
+
+
+# What is CocoaPods
+
+> CocoaPods是Swift 和 Objective-C [Cocoa](https://baike.baidu.com/item/Cocoa/7050456?fr=aladdin)项目的依赖管理工具。它拥有超过7.4万个库，并在超过300万个应用中使用。CocoaPods可以帮助你优雅地扩展项目。
+
+
+
+## Install
+
+> CocoaPods是使用Ruby构建的，并且可以使用 macOS上可用的默认Ruby安装。我们建议您使用默认的Ruby。
+>
+> 使用默认的Ruby安装可能要求您在安装gems时使用sudo。进一步安装说明请查看[指南](https://guides.cocoapods.org/using/getting-started.html#getting-started).
+
+```
+$ sudo gem install cocoapods
+```
+
