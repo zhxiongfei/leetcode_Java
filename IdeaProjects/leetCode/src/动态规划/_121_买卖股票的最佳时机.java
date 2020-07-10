9685124839a03c7cf15a837ea462f18f0190f173
zhangxiongfei <zhangxiongfei@cogo.club>
2020-07-10 16:56:33 +0800

    买卖股票

--- IdeaProjects/leetCode/src/动态规划/_121_买卖股票的最佳时机.java
+++ IdeaProjects/leetCode/src/动态规划/_121_买卖股票的最佳时机.java
@@ -1,18 +1,23 @@
 package 动态规划;
 
+import java.util.Stack;
+
 public class _121_买卖股票的最佳时机 {
 
+    /**
+
+     暴力法
+     遍历每一种可能
+     时间复杂度 : O(n ^ 2)
+     空间复杂度 : O(1)
+
+     */
     public int maxProfit(int[] prices) {
         if (prices == null || prices.length == 0) return 0;
         if (prices.length == 1) return 0;
 
-        return max(prices,prices.length - 1);
-    }
-
-    // 暴力两层循环
-    private int max1(int[] prices, int end){
         int max = 0;
-        for (int i = 1; i <= end; i++) {
+        for (int i = 1; i < prices.length; i++) {
 
             for (int j = 0; j < i; j++) {
                 if (prices[i] <= prices[j]) continue;
@@ -23,64 +28,68 @@ public class _121_买卖股票的最佳时机 {
         return max;
     }
 
-    // 动态规划 记忆数组
-    private int max2(int[] prices, int end){
-        int[] dp = new int[end + 1];
-        dp[0] = 0;
-        dp[1] = prices[1] - prices[0];
+    /**
 
-        int max = dp[1];
-        for (int i = 2; i <= end; i++) {
-            int tmp = prices[i] - prices[i - 1];
-            dp[i] = Math.max(dp[i - 1] + tmp,tmp);
+     动态规划
+     记录最小价值 min 为数组首元素
+     记录最大获利润 res
 
-            max = Math.max(dp[i],max);
+     从 index为1 元素开始遍历
+     当最小值 > prices[i] 时, 说明当前下标没有利润。 将 minprice = prices[i] 即可
+     当最小值 < prices[i] 时, maxprofit = max(maxprofit,prices[i] - minprice);
 
-            System.out.println(max);
-        }
-        return max < 0 ? 0 : max;
-    }
+     时间复杂度 : O(n)
+     空间复杂度 : O(1)
 
-    // 动态规划 记录变量
-    private int max(int[] prices, int end){
-
-        int cur = prices[0] - prices[1] > 0 ? prices[0] - prices[1] : 0;
-        int max = cur;
-
-        for (int i = 2; i <= end; i++) {
-            int tmp = prices[i] - prices[i - 1];
-            cur = Math.max(cur + tmp,tmp);
-
-            max = Math.max(cur,max);
-
-            System.out.println(max);
-        }
-        return max;
-    }
-
-
+     */
     public int maxProfit1(int[] prices) {
-
         if (prices.length <= 1) return 0;
-
-        int[] dp = new int[prices.length];
-        dp[0] = 0;
-
-        int max = 0;
-        int min = prices[0];
+        int maxprofit = 0;
+        int minprice = prices[0];
 
         for (int i = 1; i < prices.length; i++) {
 
-            if (min > prices[i]){
-                dp[i] = 0;
-                min = prices[i];
-
+            if (minprice > prices[i]){
+                minprice = prices[i];
                 continue;
             }
+            maxprofit = Math.max(maxprofit, prices[i] - minprice);
+        }
+        return maxprofit;
+    }
 
-            max = Math.max(max, prices[i] - min);
+    /**
+     *
+     * next greater number问题
+     * 使用单调栈
+     *
+     * 可以解决问题，顺便复习一下单调栈的用法也不错
+     * 但是个人感觉，用的比较牵强。
+     * 本可以使用一个 minprice 解决的问题
+     * 何必使用一个栈来解决呢？ 并且栈上，貌似最多存放的元素也是 1 个
+     * 相当于 用 stack 来存放 minprice
+     *
+     * */
+
+    public int maxProfit2(int[] prices){
+        if (prices == null || prices.length <= 1) return 0;
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(prices[0]);
+
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int price = prices[i];
+
+            while (!stack.isEmpty() && price < stack.peek()){
+                stack.pop();
+            }
+
+            if (stack.isEmpty()) stack.push(price);
+
+            maxprofit = Math.max(maxprofit, price - stack.peek());
         }
 
-        return max;
+        return maxprofit;
     }
 }
