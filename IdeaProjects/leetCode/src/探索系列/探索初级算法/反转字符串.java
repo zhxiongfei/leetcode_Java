be2b21437bcd8d7f8e5c8cddf518f71f8579f6a2
zhangxiongfei <zhangxiongfei@cogo.club>
2020-07-28 14:33:49 +0800

    递归 反转字符串

--- IdeaProjects/leetCode/src/探索系列/探索初级算法/反转字符串.java
+++ IdeaProjects/leetCode/src/探索系列/探索初级算法/反转字符串.java
@@ -2,7 +2,12 @@ package 探索系列.探索初级算法;
 
 public class 反转字符串 {
 
-    public void reverseString(char[] s) {
+    /**
+     *
+     * 迭代
+     *
+     * */
+    public void reverseString1(char[] s) {
         if (s == null || s.length <= 1) return;
 
         int left = 0, right = s.length - 1;
@@ -16,4 +21,22 @@ public class 反转字符串 {
         }
     }
 
+    /**
+     *
+     * 递归
+     *
+     * */
+    public void reverseHelper(char[] s, int i, int j){
+        if (i >= j) return;
+        reverseHelper(s, i + 1, j - 1);
+
+        char c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+
+    public void reverseString(char[] s){
+        if (s.length <= 1) return;
+        reverseHelper(s, 0, s.length - 1);
+    }
 }
